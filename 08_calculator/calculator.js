edb51b94f3f5bef3d48f757ded120176bd603acc
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-08 09:24:55 -0600

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
 const add = function() {
+  const arrayNumbers = [...arguments];
+
+    let sum = 0;
+
+    for (let i of arrayNumbers) {
+
+      sum += i;
+    }
+
+   
+    return parseInt(sum);
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2
+};
+
+const sum = function(array) {
+  let sum = 0;
+  for (let i of array) {
+    sum+=i;
+  }
+  return sum;
+
 	
 };
 
-const subtract = function() {
+const multiply = function(array) {
+  let mult = 1;
+  for (let i of array) {
+    mult *= i;
+  }
+  return mult;
+};
+
+const power = function(num1,num2) {
+  return Math.pow(num1,num2)
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(number) {
+  let fact = 1
+  if (number > 0) {
+    for (let i = 1; i <= number; i++) {
+      fact*=i;
+  
+    } 
+  }
+  return fact;
 	
 };
 
