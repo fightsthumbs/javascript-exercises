fc5e5c1c4d9c2f286a7016dc74a4f3e1cf3c7e5d
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-07 21:38:18 -0600

    complete remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+  const myArguments = Array.from(arguments);
+  const myArray = myArguments.shift(); //first array
+  const removableItems = [...myArguments] //rest of arguments
+  const result = myArray.filter((x) => {
+    if (removableItems.indexOf(x) < 0) { // si la var x no tiene un index en la lista de removibles, devuelvalo
+      return x;
+    } 
+  })
+ 
+  return result;
 };
 
 // Do not edit below this line
