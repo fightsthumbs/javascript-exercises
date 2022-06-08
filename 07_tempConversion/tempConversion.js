de916113f681a759972c18b41c2a6607439e240b
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-08 07:52:24 -0600

    tempConversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
+
+const toFixedNum = (value, decimal=1) => {
+  if (typeof value === "number") {
+    return parseFloat(value.toFixed(decimal));
+
+  }
+}
+
+const ftoc = function(fh) {
+  const celsius = (fh - 32) * (5/9);
+  const convertedNum = toFixedNum(celsius);
+  return convertedNum;
 
 };
 
-const ctof = function() {
-
+const ctof = function(cl) {
+  const farenheit = (cl * (9/5) + 32)
+  const convertedNum = toFixedNum(farenheit);
+  return convertedNum;
 };
 
 // Do not edit below this line
