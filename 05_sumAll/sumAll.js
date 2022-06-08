9cd1deb179c2b31bfd73c48bb46d0d58e693c967
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-07 21:52:24 -0600

    sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
+
+if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+  return "ERROR";
+} 
+  
+
+  for (let i = (num1 < num2 ? num1:num2); i <= (num1<num2 ? num2:num1); i++) {
+    sum += i;
+  }
+  return sum
 
 };
 
