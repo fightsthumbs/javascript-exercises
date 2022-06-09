733e53e79ece6348262984ce281f50069b28d943
Benjamín Álvarez Rodríguez <fightsthumbs@gmail.com>
2022-06-08 20:21:14 -0600

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let num1 = 0
+  let num2 = 1
+  let sum;
+
+  if (number < 0) {
+    return "OOPS"
+  } 
+  for (let i = 1; i < number; i++) {
+    sum = num1 + num2;
+    num1 = num2
+    num2= sum
+  }
+  return num2;
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
